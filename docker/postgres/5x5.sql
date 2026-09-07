0288058ce35198b985af1ae82583e99bfe189f45
José Otávio Ávila Alves Pires <jose.otavio2707@gmail.com>
2026-09-07 16:27:30 -0300

    fix(canStillMatch method fixed)

--- docker/postgres/5x5.sql
+++ docker/postgres/5x5.sql
@@ -1,99 +1,25 @@
-INSERT INTO puzzles (
-            size, 
-            row_clues, 
-            col_clues
-           )
-    VALUES (5,
-            '[[1],[3],[5],[3],[1]]',
-            '[[1],[3],[5],[3],[1]]'
-           );
+    5,
+    '[[1],[2],[3],[4],[5]]',
+    '[[1],[2],[3],[4],[5]]'
+);
 
-INSERT INTO puzzles (
-            size, 
-            row_clues, 
-            col_clues
-           )
-    VALUES (5,
-            '[[5],[1,1],[1,1],[1,1],[5]]',
-            '[[5],[1,1],[1,1],[1,1],[5]]'
-           );
+INSERT INTO puzzles (size, row_clues, col_clues)
+VALUES (
+    5,
+    '[[5],[4],[3],[2],[1]]',
+    '[[5],[4],[3],[2],[1]]'
+);
 
-INSERT INTO puzzles (
-            size, 
-            row_clues, 
-            col_clues
-           )
-    VALUES (5,
-            '[[2],[4],[1,1],[4],[2]]',
-            '[[2],[4],[1,1],[4],[2]]'
-           );
+INSERT INTO puzzles (size, row_clues, col_clues)
+VALUES (
+    5,
+    '[[5],[1],[1],[1],[1]]',
+    '[[1],[1],[5],[1],[1]]'
+);
 
-INSERT INTO puzzles (
-            size, 
-            row_clues, 
-            col_clues
-           )
-    VALUES (5,
-            '[[1,1],[3],[5],[3],[1,1]]',
-            '[[1,1],[3],[5],[3],[1,1]]'
-           );
-
-INSERT INTO puzzles (
-            size, 
-            row_clues, 
-            col_clues
-           )
-    VALUES (5,
-            '[[3],[1,1],[1,1,1],[1,1],[3]]',
-            '[[3],[1,1],[1,1,1],[1,1],[3]]'
-           );
-
-INSERT INTO puzzles (
-            size, 
-            row_clues, 
-            col_clues
-           )
-    VALUES (5,
-            '[[1],[1,1],[5],[1,1],[1]]',
-            '[[1],[1,1],[5],[1,1],[1]]'
-           );
-
-INSERT INTO puzzles (
-            size, 
-            row_clues, 
-            col_clues
-           )
-    VALUES (5,
-            '[[2,1],[1,2],[1],[2,1],[1,2]]',
-            '[[2,1],[1,2],[1],[2,1],[1,2]]'
-           );
-
-INSERT INTO puzzles (
-            size, 
-            row_clues, 
-            col_clues
-           )
-    VALUES (5,
-            '[[1,1],[1,1],[1],[1,1],[1,1]]',
-            '[[1,1],[1,1],[1],[1,1],[1,1]]'
-           );
-
-INSERT INTO puzzles (
-            size, 
-            row_clues, 
-            col_clues
-           )
-    VALUES (5,
-            '[[4],[1,1],[2],[1,1],[4]]',
-            '[[2],[1,1,1],[1,1,1],[1,1,1],[2]]'
-           );
-
-INSERT INTO puzzles (
-            size, 
-            row_clues, 
-            col_clues
-           )
-    VALUES (5,
-            '[[1,2],[3],[1,1],[3],[2,1]]',
-            '[[1,2],[3],[1,1],[3],[2,1]]'
-           );
+INSERT INTO puzzles (size, row_clues, col_clues)
+VALUES (
+    5,
+    '[[1],[3],[5],[3],[1]]',
+    '[[1],[3],[5],[3],[1]]'
+);
